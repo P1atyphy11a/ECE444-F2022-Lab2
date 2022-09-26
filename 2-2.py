eeee3b604525c3a1247036b661c8b86ad4572cc2
Haoyu Li <haoy.li@mail.utoronto.ca>
2022-09-26 10:07:08 -0400

    modified 2-1.py and created 2-2.py to reproduce examples

--- 2-2.py
+++ 2-2.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/usr/<name>')
+
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
 app.add_url_rule('/','index',index)
